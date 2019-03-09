81c7d54f38aa7170b4dc48b72e74ae8f9c5ed8f9
rafalcurylo <rafal.curylo@gmail.com>
2019-03-09 14:21:47 +0100

    jax rs client & server

--- jaxrsserver/src/main/java/com/rafalcurylo/ee8/jaxrs/server/PeopleResource.java
+++ jaxrsserver/src/main/java/com/rafalcurylo/ee8/jaxrs/server/PeopleResource.java
@@ -1,40 +1,30 @@
 package com.rafalcurylo.ee8.jaxrs.server;
 
+import javax.ejb.EJB;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Application;
-import java.util.Arrays;
 import java.util.List;
 
 @ApplicationPath("resources")
 @Path("people")
+//@Named
+//@RequestScoped
 public class PeopleResource extends Application {
+
+   // @EJB
+ //   PeopleService service;
 
     @GET
     @Produces("application/json")
     public List<Person> getAll() {
 
-        Person john = Person.builder()
-                .firstName("John")
-                .lastName("Smith")
-                .id(1)
-                .build();
-
-        Person ann = Person.builder()
-                .firstName("Ann")
-                .lastName("Smith")
-                .id(2)
-                .build();
-
-        Person caren = Person.builder()
-                .firstName("Caren")
-                .lastName("Smith")
-                .id(2)
-                .build();
-
-        return Arrays.asList(john, ann, caren);
+        //return service.getAll();
+        return null;
     }
 
 }
---
+++ jaxrsserver/src/main/java/com/rafalcurylo/ee8/jaxrs/server/PeopleService.java
@@ -0,0 +1,34 @@
+package com.rafalcurylo.ee8.jaxrs.server;
+
+import javax.ejb.Stateless;
+import java.util.Arrays;
+import java.util.List;
+
+//@Stateless
+public class PeopleService {
+
+    public List<Person> getAll() {
+
+        Person john = Person.builder()
+                .firstName("John")
+                .lastName("Smith")
+                .id(1)
+                .build();
+
+        Person ann = Person.builder()
+                .firstName("Ann")
+                .lastName("Smith")
+                .id(2)
+                .build();
+
+        Person caren = Person.builder()
+                .firstName("Caren")
+                .lastName("Smith")
+                .id(3)
+                .build();
+
+        return Arrays.asList(john, ann, caren);
+
+    }
+
+}
